07372a2d3b02ea4ef30f2b1e83e37f17640bc631
Małgorzata Kwiecień <kwiecien.malgorzata.00@wp.pl>
2023-04-18 01:53:52 +0200

    made changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const continentSelect = document.querySelector('#continent')
+const continentSelect = document.getElementById('continent')
 const amountInput = document.querySelector('#amount')
 
 const error = document.querySelector('.error-text')
@@ -7,26 +7,23 @@ const clearAllBtn = document.querySelector('.clear')
 const submitBtn = document.querySelector('.submit')
 
 const countryResults = document.querySelector('.country__results')
-const countryBox = document.querySelector('country__box')
+const countryBox = document.getElementById('country__box')
 
 const modalShadow = document.querySelector('.modal-shadow')
 const closeModalBtn = document.querySelector('.close')
 
-const countryName = document.querySelector('.name')
-const capital = document.querySelector('.capital')
-const population = document.querySelector('.population')
-const currency = document.querySelector('.cureency')
-const subregion = document.querySelector('.subregion')
-const languages = document.querySelector('.languages')
+const countryName = document.getElementsByClassName('name')
+const capital = document.getElementsByClassName('capital')
+const population = document.getElementsByClassName('population')
+const currency = document.getElementsByClassName('cureency')
+const subregion = document.getElementsByClassName('subregion')
+const languages = document.getElementsByClassName('languages')
 
 let ID = 0
 let validAmount
 let usersValue
-let selectedContinent
-let chosenContinent
-let allCountries
-let newArr = []
-let arr2 = []
+let countryList = []
+let countryCard
 
 const URL = 'https://countries.trevorblades.com/graphql'
 
@@ -44,43 +41,61 @@ const checkForm = () => {
 	}
 }
 
-async function countrySelection() {
-	fetch(URL, {
+function queryFetch(query, variables) {
+	return fetch(URL, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({
-			query: `
-          query {
-            continents {
-              code
-              name
-              countries {
-                name
-              }
-            }
-          }
-        `,
+			query: query,
+			variables: variables,
 		}),
-	})
-		.then(res => res.json())
-		.then(data => {
-			data.data.continents.forEach(continent => {
-				if (selectedContinent === continent.code) {
-					chosenContinent = continent
-				}
-			})
+	}).then(res => res.json())
+}
 
-			allCountries = chosenContinent.countries
-			allCountries.forEach(country => {
-				// console.log(country.name)
-				arr2.push(country.name)
-			})
-			console.log(arr2)
-			newArr = arr2.map(el => el.replace('""', ''))
-			console.log(newArr)
-		})
+queryFetch(`
+    query {
+    continents {
+        code
+        name
+    }
+}
+`).then(data => {
+	data.data.continents.forEach(continent => {
+		const option = document.createElement('option')
+		option.value = continent.code
+		option.innerText = continent.name
+		continentSelect.append(option)
+	})
+})
+
+function getContinentCountries(continentCode) {
+	return queryFetch(
+		`
+    query getCountries($code: ID!) {
+        continent(code: $code) {
+            countries {
+                name
+                code
+            }
+        }
+    }
+    `,
+		{ code: continentCode }
+	).then(data => {
+		return data.data.continent.countries
+	})
+}
+
+const showModal = () => {
+	if (!(modalShadow.style.display === 'block')) {
+		modalShadow.style.display = 'block'
+	} else {
+		modalShadow.style.display = 'none'
+	}
+
+	modalShadow.classList.toggle('modal-animation')
 }
 
 function selectValue() {
@@ -88,15 +103,48 @@ function selectValue() {
 	console.log(usersValue)
 }
 
-const selectContinent = () => {
-	selectedContinent = continent.options[continent.selectedIndex].value
-}
-
 const clearInputs = () => {
 	continentSelect.selectedIndex = 0
 	amountInput.value = ''
 	error.textContent = ''
+	countryBox.innerHTML = ''
+	countryResults.style.display = 'none'
 }
 
+continentSelect.addEventListener('change', async e => {
+	const continentCode = e.target.value
+	const countries = await getContinentCountries(continentCode)
+	countryResults.style.display = 'block'
+	countryBox.innerHTML = ''
+	countries.forEach(country => {
+		// const element = document.createElement('div')
+		// element.innerText = country.name
+		// countryBox.append(element)
+		countryCard = document.createElement('div')
+		countryCard.classList.add('country__card')
+		countryCard.setAttribute('id', ID)
+		countryCard.innerHTML = `
+        <img class="country__card-flag" src="https://flagcdn.com/${country.code.toLowerCase()}.svg" loading="lazy" alt="flag of a ${
+			country.name
+		}">
+
+        <div class="country__card-info">
+            <h3 class="country__card-name">${country.name}</h3>
+            <p class="country__card-details" onclick='showModal()'><i class="fa-solid fa-circle-info"></i></p>
+        </div>`
+		countryBox.append(countryCard)
+
+		ID++
+	})
+})
+
+closeModalBtn.addEventListener('click', showModal)
 submitBtn.addEventListener('click', checkForm)
 clearAllBtn.addEventListener('click', clearInputs)
+
+// TO DO
+// MODAL - displaying info of each country (each has its own unique id)
+// NUMBER OF DIVS - displaying the number of countries according to the number entered by the user
+// LINKING - connecting to the function with validation, when selecting a select and entering a number, only then the proper function will be called
+// ADD LOADING INFO
+// update readme with instructions
