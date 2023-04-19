9672b5d934491b66df16dc77a75d42c48a14c916
Małgorzata Kwiecień <kwiecien.malgorzata.00@wp.pl>
2023-04-19 21:06:54 +0200

    replaced if statements with conditional operators

--- src/js/main.js
+++ src/js/main.js
@@ -161,40 +161,20 @@ const displayMoreInfo = async chosenCountry => {
 
 	const response = await fetch(`https://restcountries.com/v3.1/alpha/${code}`)
 	const data = await response.json()
-	console.log(data)
 
 	countryName.textContent = data[0].name.common || 'No information found!'
 
-	if (data[0].capital) {
-		capital.textContent = data[0].capital || 'No information found!'
-	} else {
-		capital.textContent = 'No information found!'
-	}
+	capital.textContent = data[0].capital ? data[0].capital : 'No information found!'
 
-	if (data[0].population) {
-		population.textContent = data[0].population || 'No information found!'
-	} else {
-		population.textContent = 'No information found!'
-	}
+	population.textContent = data[0].population ? data[0].population : 'No information found!'
 
-	if (data[0].currencies) {
-		currency.textContent =
-			`${Object.values(data[0].currencies)[0].name}, ${Object.values(data[0].currencies)[0].symbol}` || 'No information found!'
-	} else {
-		currency.textContent = 'No information found!'
-	}
+	currency.textContent = data[0].currencies
+		? `${Object.values(data[0].currencies)[0].name}, ${Object.values(data[0].currencies)[0].symbol}`
+		: 'No information found!'
 
-	if (data[0].subregion) {
-		subregion.textContent = data[0].subregion || 'No information found!'
-	} else {
-		subregion.textContent = 'No information found!'
-	}
+	subregion.textContent = data[0].subregion ? data[0].subregion : 'No information found!'
 
-	if (data[0].languages) {
-		languages.textContent = Object.values(data[0].languages).join(', ') || 'No information found!'
-	} else {
-		languages.textContent = 'No information found!'
-	}
+	languages.textContent = data[0].languages ? Object.values(data[0].languages).join(', ') : 'No information found!'
 }
 
 const cleanInfo = () => {
